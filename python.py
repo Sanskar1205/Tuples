a37ff34c66c71ec9bcf3de409e1e486771833242
Sanskar <sanskarmp1215@gmail.com>
2024-05-05 10:09:10 +0530

    add my python file

---
+++ python.py
@@ -0,0 +1,33 @@
+tuple1=(2,3,4,5,4,1,1,7,8,2,2)
+tuple2=("red", "blue", "green","pink","yellow","purple","white","black","grey")
+print(tuple1)
+print(tuple2)
+
+
+# index positive tuple 
+# print(tuple2[0])
+# print(tuple2[1])
+# print(tuple2[3])
+
+# negative indexing
+
+# if "red" in tuple2:
+#     print("red is present")
+# else:
+#     print("red is not present")
+
+# print(tuple2[3:5:7])
+# print(tuple2[1:8:3])
+# print(tuple2[0:8:2])
+# print(tuple2[0:8:4])
+# print(tuple2[0:8:2])
+# print(tuple2[0:8:2])
+# print(tuple2[-1:])
+# print(tuple2[:])
+# print(tuple2[0:-8:-2])
+
+
+
+
+
+
